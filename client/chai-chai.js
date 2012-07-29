5f074fab72fb370679617c0785487c1339e97549
leodt <leodt@debian>
2012-07-29 23:37:15 +0800

    update

--- client/chai-chai.js
+++ client/chai-chai.js
@@ -1,5 +1,6 @@
+var Profile = amplify.store("profile") || amplify.store("profile", {"username": ""});
 
-Session.set("user_id", null);
+Session.set("user_id", Profile.username || null);
 
 Session.set("room_id", null);
 
@@ -13,10 +14,37 @@ Meteor.subscribe("rooms", function() {
     }
 });
 
+function login(username){
+    Session.set("user_id", username);
+
+    amplify.store("profile", {"username": Session.get("user_id")});
+}
+
+///////// profile /////////
+Template.profile.events = {
+    'keyup #username': function(e){
+	if(e.keyCode == 13){
+	    if(!e.target.value){
+		alert("username must be inputed.");
+	    }
+	    else{
+		login(e.target.value);
+	    }
+	}
+    }
+};
+Template.profile.logined = Session.get("user_id") ? "logined" : "login";
+Template.profile.username = Session.get("user_id");
 
 ///////// rooms /////////
 Template.rooms.rooms = function(){
     return Room.find();
+};
+Template.rooms.in_room = function(){
+    if(this._id == Session.get("room_id")){
+	return true;
+    }
+    return false;
 };
 Template.rooms.events = {
     'click .enter': function(evt){
@@ -30,16 +58,25 @@ Template.rooms.events = {
 Template.chats.chats = function(){
     return Chat.find({room_id: Session.get("room_id")});
 };
-
+Template.chats.updated = function(){
+    return this.updated.replace("T", " ").replace("Z", "");
+};
+Template.chats.content = function(){
+    console.log(this.content);
+    return this.content.replace(/(\#.*?\#)/ig, '<a class=\"chat-tags\" href="#">$1</a>');
+};
 
 /////// chat input ///////////
 Template.chat_input.events = {
     "keyup #chat-text": function(e){
-        if(e.keyCode == 13){
+        if(e.ctrlKey && e.keyCode == 13 && e.target.value){
 	    Chat.insert({
 	        room_id: Session.get("room_id"),
-	        content: e.target.value
+	        content: e.target.value,
+		user_id: Session.get("user_id"),
+		updated: new Date()
             });
+	    e.target.value = "";
         }
     }
 };
